9a0d022c162166a7e25d2399bdf5ce452a90250e
mizraimeliab <mizraimeliab168@gmail.com>
2020-07-05 01:13:08 -0500

    productionv7

--- server/index.js
+++ server/index.js
@@ -13,7 +13,9 @@ app.use(express.json());
 
 //Routes
 app.use('/users',require('./routes/users.routes'));
-
+app.get('/', function(req,res){
+    res.send("Hello world")
+})
 //Server listen
 app.listen(3000, ()=> {
     console.log("Server On Port ", app.get('port'))
