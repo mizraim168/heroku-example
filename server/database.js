dd3d51b826c34768bb5e1ed1b144b83f14e3e5d6
mizraimeliab <mizraimeliab168@gmail.com>
2020-07-05 00:48:20 -0500

    production

--- server/database.js
+++ server/database.js
@@ -3,11 +3,11 @@ const {Pool} = require('pg');
 
 // Connection to postgres
     const pool = new Pool({
-        host: 'localhost',
-        user: 'postgres',
-        password: 'postgres',
-        database: 'test',
-        port: '5432'
+        host: process.env.HOST , //'localhost'
+        user: process.env.USER, //'postgres',
+        password: process.env.PASSWD, //'postgres',
+        database: process.env.DATABASE, //'test',
+        port: process.env.PORTDB //'5432'
     
     });
     console.log('DB is coneted');
